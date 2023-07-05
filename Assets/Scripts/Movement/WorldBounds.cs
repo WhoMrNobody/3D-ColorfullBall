ecc8ef634321d750752f6bebb4156366896858ab
Emre Hün <the.emrehun@gmail.com>
2023-07-05 13:52:34 +0300

    Added WorldBounds

--- Assets/Scripts/Movement/WorldBounds.cs
+++ Assets/Scripts/Movement/WorldBounds.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldBounds : MonoBehaviour
+namespace ColorFullBall.Movements
 {
-    // Start is called before the first frame update
-    void Start()
+    public class WorldBounds : MonoBehaviour
     {
-        
-    }
+        [SerializeField] Transform _vectorTop;
+        [SerializeField] Transform _vectorBottom;
+        [SerializeField] Transform _vectorLeft;
+        [SerializeField] Transform _vectorRight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void LateUpdate()
+        {
+            HandleWorldBounds();
+        }
+
+        void HandleWorldBounds()
+        {
+            Vector3 viewPos = transform.position;
+            viewPos.z = Mathf.Clamp(viewPos.z, _vectorBottom.position.z, _vectorTop.position.z);
+            viewPos.x = Mathf.Clamp(viewPos.x, _vectorLeft.position.x, _vectorRight.position.x);
+            transform.position = viewPos;
+        }
     }
 }
+
