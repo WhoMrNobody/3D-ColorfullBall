afd355b9d597256d1b35b3579d5fda36746f8f77
Emre Hün <the.emrehun@gmail.com>
2023-07-13 11:40:01 +0300

    Added InGame Buttons Functions

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -8,6 +8,14 @@ namespace ColorFullBall.UI
     public class UIManager : MonoBehaviour
     {
         [SerializeField] Image _whiteImage;
+        [SerializeField] GameObject _settingOpen, _settingClose;
+        [SerializeField] GameObject _muteOnButton, _muteOffButton;
+        [SerializeField] GameObject _vibrationOnButton, _vibrationOffButton;
+        [SerializeField] GameObject _iapButton;
+        [SerializeField] GameObject _informationButton;
+        [SerializeField] Animator _layoutAnimator;
+
+
         int _alphaValue = 0;
 
         public IEnumerator FlashDeathEffect()
@@ -36,6 +44,45 @@ namespace ColorFullBall.UI
                 }
             }
 
+        }
+
+
+        public void OpenSettings()
+        {
+            _layoutAnimator.SetTrigger("SlideDown");
+            _settingOpen.SetActive(false);
+            _settingClose.SetActive(true);
+        }
+
+        public void CloseSettings()
+        {
+            _layoutAnimator.SetTrigger("SlideUp");
+            _settingClose.SetActive(false);
+            _settingOpen.SetActive(true);
+        }
+
+        public void MuteOnButton()
+        {
+            _muteOnButton.SetActive(false);
+            _muteOffButton.SetActive(true);
+        }
+
+        public void MuteOffButton()
+        {
+            _muteOffButton.SetActive(false);
+            _muteOnButton.SetActive(true);
+        }
+
+        public void VibrationOnButton()
+        {
+            _vibrationOnButton.SetActive(false);
+            _vibrationOffButton.SetActive(true);
+        }
+
+        public void VibrationOffButton()
+        {
+            _vibrationOffButton.SetActive(false);
+            _vibrationOnButton.SetActive(true);
         }
     }
 }
