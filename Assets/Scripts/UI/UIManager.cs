66415abf5c255c26f26adbb188fddf715553bbb9
Emre Hün <the.emrehun@gmail.com>
2023-07-14 12:16:45 +0300

    Added Settings Save System

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -12,12 +12,25 @@ namespace ColorFullBall.UI
         [SerializeField] GameObject _muteOnButton, _muteOffButton;
         [SerializeField] GameObject _vibrationOnButton, _vibrationOffButton;
         [SerializeField] GameObject _iapButton;
-        [SerializeField] GameObject _informationButton;
+        [SerializeField] GameObject _informationButton, _infoGameObject;
         [SerializeField] Animator _layoutAnimator;
 
 
         int _alphaValue = 0;
 
+        private void Start()
+        {
+            if (!PlayerPrefs.HasKey("Sound"))
+            {
+                PlayerPrefs.SetInt("Sound", 1);
+            }
+
+            if (!PlayerPrefs.HasKey("Vibration"))
+            {
+                PlayerPrefs.SetInt("Vibration", 1);
+            }
+
+        }
         public IEnumerator FlashDeathEffect()
         {   
             _whiteImage.gameObject.SetActive(true);
@@ -52,6 +65,31 @@ namespace ColorFullBall.UI
             _layoutAnimator.SetTrigger("SlideDown");
             _settingOpen.SetActive(false);
             _settingClose.SetActive(true);
+
+            if(PlayerPrefs.GetInt("Sound") == 1)
+            {
+                _muteOffButton.SetActive(false);
+                _muteOnButton.SetActive(true);
+                AudioListener.volume = 1;
+            }
+            else
+            {
+                _muteOffButton.SetActive(true);
+                _muteOnButton.SetActive(false);
+                AudioListener.volume = 0;
+            }
+
+            if(PlayerPrefs.GetInt("Vibration") == 1)
+            {
+                _muteOnButton.SetActive(true);
+                _muteOffButton.SetActive(false);
+            }
+            else
+            {
+                _muteOnButton.SetActive(false);
+                _muteOffButton.SetActive(true);
+            }
+           
         }
 
         public void CloseSettings()
@@ -65,25 +103,32 @@ namespace ColorFullBall.UI
         {
             _muteOnButton.SetActive(false);
             _muteOffButton.SetActive(true);
+            AudioListener.volume = 0f;
+            PlayerPrefs.SetInt("Sound", 0);
         }
 
         public void MuteOffButton()
         {
             _muteOffButton.SetActive(false);
             _muteOnButton.SetActive(true);
+            AudioListener.volume = 1f;
+            PlayerPrefs.SetInt("Sound", 1);
         }
 
         public void VibrationOnButton()
         {
             _vibrationOnButton.SetActive(false);
             _vibrationOffButton.SetActive(true);
+            PlayerPrefs.SetInt("Vibration", 0);
         }
 
         public void VibrationOffButton()
         {
             _vibrationOffButton.SetActive(false);
             _vibrationOnButton.SetActive(true);
+            PlayerPrefs.SetInt("Vibration", 1);
         }
+
     }
 }
 
